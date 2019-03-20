102b4857e3d26d671c7ad5ffb21b90e32fd59b83
Dmitry Mitichenko <dmitry.mitichenko@studiomobile.ru>
2019-03-21 00:28:23 +0300

    chore: solution cleanup

--- src/nys.service.data/src/NameProvider/INamePartProvider.cs
+++ src/nys.service.data/src/NameProvider/INamePartProvider.cs
@@ -5,6 +5,10 @@ namespace nys.service.data.NameProvider
 {
     public interface INamePartProvider
     {
+        /// <summary>
+        /// Fetch name part variants from the implementing source
+        /// </summary>
+        /// <returns></returns>
         Task<string[]> GetAsync(CancellationToken cancellationToken);
     }
 }
--- src/nys.service.data/src/NameProvider/INameProvider.cs
+++ src/nys.service.data/src/NameProvider/INameProvider.cs
@@ -5,6 +5,12 @@ namespace nys.service.data.NameProvider
 {
     public interface INameProvider
     {
+        /// <summary>
+        /// Generate next {count} name variants
+        /// </summary>
+        /// <param name="count">The requested quantity of name variants</param>
+        /// <param name="ct"></param>
+        /// <returns>Array of generated names</returns>
         Task<string[]> GetNext(int count, CancellationToken ct);
     }
 }
